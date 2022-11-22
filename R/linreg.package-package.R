22f8962f8c09904744e0e37bab8343c59a6969a6
Grace Tiernon <gtiernon@umich.edu>
2022-11-22 15:39:21 -0500

    add src and R file for rccp

---
+++ R/linreg.package-package.R
@@ -0,0 +1,9 @@
+## usethis namespace: start
+#' @useDynLib linreg.package, .registration = TRUE
+## usethis namespace: end
+NULL
+
+## usethis namespace: start
+#' @importFrom Rcpp sourceCpp
+## usethis namespace: end
+NULL
