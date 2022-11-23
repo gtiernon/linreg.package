daed2ecc639f7d545b022aec9019e82b7d3b543c
Grace Tiernon <gtiernon@umich.edu>
2022-11-22 22:53:58 -0500

    delete bonus Qs & update linreg & add tutorial

--- R/linreg.R
+++ R/linreg.R
@@ -8,45 +8,45 @@ linreg <- function(formula, data){
 
   #extract name of predictor variable(s) based on input
   #account for multiple predictors
-  if(formula[[3]][[1]] == "+"){
-    if(length(formula[[3]]==3 & length(formula[[3]][[2]])==1)){
+  if(length(formula[[3]])==1){
+    predictor = formula[[3]]
+  }else if(formula[[3]][[1]] == "+"){
+    if(length(formula[[3]])==3 & length(formula[[3]][[2]])==1){
       predictor = c(as.character(formula[[3]][[2]]), as.character(formula[[3]][[3]]))
     }
-    if(length(formula[[3]]==3 & length(formula[[3]][[2]][[2]])==1)){
-      predictor = c(as.character(formula[[3]][[2]][[2]]), as.character(formula[[3]][[3]]))
+    else if(length(formula[[3]])==3 & length(formula[[3]][[2]][[2]])==1){
+      predictor = c(as.character(formula[[3]][[2]][[2]]), as.character(formula[[3]][[2]][[3]]),
+                    as.character(formula[[3]][[3]]))
     }
-    if(length(formula[[3]]==3 & length(formula[[3]][[2]][[2]][[2]])==1)){
+    else if(length(formula[[3]])==3 & length(formula[[3]][[2]][[2]][[2]])==1){
       predictor = c(as.character(formula[[3]][[2]][[2]][[2]]), as.character(formula[[3]][[2]][[2:3]]),
                     as.character(formula[[3]][[2]][[3]]), as.character(formula[[3]][[3]]))
     }
-    if(length(formula[[3]]==3)){
+    else if(length(formula[[3]])==3){
       predictor = c(as.character(formula[[3]][[2]][[2]][[2]][[2]]), as.character(formula[[3]][[2]][[2]][[2:3]]),
                     as.character(formula[[3]][[2]][[2:3]]), as.character(formula[[3]][[2]][[3]]),
                     as.character(formula[[3]][[3]]))
     }
-  }else{
-    predictor = formula[[3]]
   }
-  predictor = as.character(predictor)
+  #predictor = as.character(predictor)
 
   #set up matrices
-  out = data %>% select(outcome)
+  out = data %>% select(all_of(outcome))
 
-  if(predictor=="."){
-    pred = data %>% select(-outcome)
-  }else{
-    pred = data %>% select(predictor)
-  }
+  if(formula[[3]]=="."){
+    pred = data %>% select(-all_of(outcome))
+  }else{ pred = data %>% select(all_of(predictor))}
 
   y = matrix(out[[1]], nrow = nrow(data))
-  X = cbind(1, as.matrix(x = pred))
+  X = cbind(1, as.matrix(pred))
   colnames(X)[1] <- "(Intercept)"
 
   #estimated coeff
   beta = solve(t(X)%*%X)%*%t(X)%*%y
 
   #final result as a list to replicate lm()
-  output = list("Call" = match.call(), "Coefficients" = t(round(beta,4)))
+  output = list("Call" = match.call(), "Coefficients" = t(round(beta,5)))
 
   return(output)
 }
+
--- R/linreg.package-package.R
+++
@@ -1,9 +0,0 @@
-## usethis namespace: start
-#' @useDynLib linreg.package, .registration = TRUE
-## usethis namespace: end
-NULL
-
-## usethis namespace: start
-#' @importFrom Rcpp sourceCpp
-## usethis namespace: end
-NULL
